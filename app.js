7d75457aae9f382a20f110e334b7869d8a970c91
cbaron <topherbaron@gmail.com>
2016-01-28 17:53:08 -0500

    adding http support

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var fs = require('fs');
 
 require('node-env-file')( __dirname + '/.env' );
 
-require('https')
-    .createServer( { key: fs.readFileSync( process.env.SSLKEY ), cert: fs.readFileSync( process.env.SSLCERT ) }, require('./router') )
-    .listen( process.env.PORT || 1337 )
+if( process.env.HTTPS === "false" ) {
+    require('http')
+        .createServer( require('./router') )
+        .listen( process.env.PORT || 1337 )
+} else {
+    require('https')
+        .createServer( { key: fs.readFileSync( process.env.SSLKEY ), cert: fs.readFileSync( process.env.SSLCERT ) }, require('./router') )
+        .listen( process.env.PORT || 1337 )
+}
