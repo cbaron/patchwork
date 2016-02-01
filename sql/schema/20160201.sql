302bec065e968f32eb50b8cdc6b1142df74ef2d5
Scott Parton <saparton@gmail.com>
2016-02-01 12:57:13 -0500

    push for user debug

---
+++ sql/schema/20160201.sql
@@ -0,0 +1,11 @@
+CREATE TABLE internshipduty (
+    id             SERIAL PRIMARY KEY,
+    name           VARCHAR(100),
+    text           VARCHAR(100)
+);
+
+CREATE TABLE internshipqualification (
+    id             SERIAL PRIMARY KEY,
+    name           VARCHAR(100),
+    text           VARCHAR(100)
+);
