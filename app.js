4f4a4c913b4720511d748b0091793f4565fb893a
cbaron <topherbaron@gmail.com>
2016-02-18 14:23:50 -0500

    fixing http bug

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var fs = require('fs'),
-    port = process.env.PORT || 1337
+var fs = require('fs'), port
 
 require('node-env-file')( __dirname + '/.env' );
+
+port = process.env.PORT || 80
 
 require('http').createServer( ( request, response ) => {
     response.writeHead( 301, { 'Location': require('util').format( 'https://%s%s', process.env.DOMAIN, request.url ) } )
