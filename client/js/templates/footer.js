0f493475e9fa675ec25d249b4776c4accf5ea15b
cbaron <topherbaron@gmail.com>
2016-02-19 00:23:12 -0500

    style improvements

--- client/js/templates/footer.js
+++ client/js/templates/footer.js
@@ -21,7 +21,7 @@ return Handlebars.template({"1":function(container,depth0,helpers,partials,data)
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.home : depth0)) != null ? stack1.footerLabel : stack1), depth0))
     + "</li>        "
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.fields : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "    </ul><hr><div class=\"future-days\"><p>A <a href='mailto:topher.baron@gmail.com'>FutureDays </a>site</p></div></footer>";
+    + "    </ul><hr><div class=\"future-days\"><p>A <a href='mailto:topher.baron@gmail.com'>FutureDays</a> site</p></div></footer>";
 },"useData":true});
 
 };
--- client/js/templates/home.js
+++ client/js/templates/home.js
@@ -3,7 +3,7 @@ module.exports = function(Handlebars) {
 return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression;
 
-  return "<div data-js=\"container\" class=\"home-class client-view\"><h4>Fresh food from farmers you know!</h4><h3><a href=\"https://docs.google.com/forms/d/1aBjdeNcfd9Xa8XyLlM_tqg5NT_IUZVyluj4a5oC5M94/viewform\" target=\"_blank\">2016 CSA Signup Form!</a></h3><div data-js=\"carousel\" id=\"carousel\" class=\"carousel slide\"><div data-js=\"carouselInner\" class=\"carousel-inner\" role=\"listbox\"><div class=\"item active\"><img src=\"/static/img/"
+  return "<div data-js=\"container\" class=\"home-class client-view\"><h4>Fresh food from farmers you know!</h4><h3><a href=\"https://docs.google.com/forms/d/1aBjdeNcfd9Xa8XyLlM_tqg5NT_IUZVyluj4a5oC5M94/viewform\" target=\"_blank\">2016 CSA Sign-up Form!</a></h3><div data-js=\"carousel\" id=\"carousel\" class=\"carousel slide\"><div data-js=\"carouselInner\" class=\"carousel-inner\" role=\"listbox\"><div class=\"item active\"><img src=\"/static/img/"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.image : depth0)) != null ? stack1.path : stack1), depth0))
     + "\" alt=\""
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.image : depth0)) != null ? stack1.description : stack1), depth0))
