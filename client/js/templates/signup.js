6bbc66ea30736bb2e8f3094b8c2e5dd89ee9961f
Scott Parton <saparton@gmail.com>
2016-02-21 22:03:29 -0500

    share selection page redesigned

--- client/js/templates/signup.js
+++ client/js/templates/signup.js
@@ -1,7 +1,7 @@
 module.exports = function(Handlebars) {
 
 return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div data-js=\"container\" class=\"signup-class col-sm-12\"><div class=\"row intro\"><div>Sign up for our CSA</div></div><hr><div class=\"walkthrough-container\"><div data-js=\"walkthrough\" class=\"row\"></div><div class=\"row nav\"><div class=\"col-xs-6 text-center\"><span data-js=\"leftBtn\" class=\"glyphicon glyphicon-chevron-left\"></span></div><div class=\"col-xs-6 text-center\"><span data-js=\"rightBtn\" class=\"glyphicon glyphicon-chevron-right\"></span></div></div></div></div>";
+    return "<div data-js=\"container\" class=\"signup-class col-sm-12\"><div class=\"row intro\"><div>Welcome to our CSA Singup Page</div></div><hr><div class=\"walkthrough-container\"><div data-js=\"walkthrough\" class=\"row\"></div><div class=\"row nav\"><div class=\"col-xs-6 text-center\"><span data-js=\"leftBtn\" class=\"glyphicon glyphicon-chevron-left\"></span></div><div class=\"col-xs-6 text-center\"><span data-js=\"rightBtn\" class=\"glyphicon glyphicon-chevron-right\"></span></div></div></div></div>";
 },"useData":true});
 
 };
