765167ed8cd2f44666c09ce0c6b26d9f946996ca
Scott Parton <saparton@gmail.com>
2016-02-21 23:07:10 -0500

    refined share page, changed mobile styling

--- client/js/templates/signup/share.js
+++ client/js/templates/signup/share.js
@@ -5,7 +5,7 @@ return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,
 
   return "<li class=\"share\" data-id=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\" data-js=\"container\"><div class=\"row\"><div class=\"share-label vcenter col-sm-3\"><div>"
+    + "\"><div class=\"row\"><div data-js=\"container\" class=\"share-label vcenter col-sm-4\"><div>"
     + alias4(((helper = (helper = helpers.label || (depth0 != null ? depth0.label : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"label","hash":{},"data":data}) : helper)))
     + "</div><div><span>"
     + alias4(((helper = (helper = helpers.humanStartdate || (depth0 != null ? depth0.humanStartdate : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"humanStartdate","hash":{},"data":data}) : helper)))
@@ -13,7 +13,7 @@ return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,
     + alias4(((helper = (helper = helpers.humanEnddate || (depth0 != null ? depth0.humanEnddate : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"humanEnddate","hash":{},"data":data}) : helper)))
     + "</span></div><div>"
     + alias4(((helper = (helper = helpers.duration || (depth0 != null ? depth0.duration : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"duration","hash":{},"data":data}) : helper)))
-    + " weeks</div></div><div class=\"share-description vcenter col-sm-9\">"
+    + " weeks</div></div><div class=\"share-description vcenter col-sm-8\">"
     + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
     + "</div></div></li>";
 },"useData":true});
