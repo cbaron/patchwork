0aceb615eb634bd711f8800257be7f96d34f41bc
ScottAP108 <saparton@gmail.com>
2017-10-02 09:59:52 -0700

    minor

--- client/js/templates/signup/shares.js
+++ client/js/templates/signup/shares.js
@@ -2,7 +2,7 @@ module.exports = p =>
 `<div class="Shares" data-js="container">
     <p>As a member in our CSA program, you will receive a weekly box of fresh-picked, chemical-free produce all grown at Patchwork Gardens, right here in the Miami Valley.</p>
     <p>If you’re not quite sure you’re ready for a CSA, check out our <span data-js="csaInfoBtn" class="link">CSA Program</span> page for more information</p>
-    <h2 data-js="header">Please select which season(s) you would like to join us</h2>
+    <h2>Please select which season(s) you would like to join us</h2>
     <ul data-js="shares"></ul>
     <div class="error">Please select at least one share.</div>
 </div>`
