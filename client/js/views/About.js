f0c887f184d6c39518338820ba364c7331141cde
ScottAP108 <saparton@gmail.com>
2018-04-08 17:32:01 -0700

    staff photos cleaned up, warping fixed

--- client/js/views/About.js
+++ client/js/views/About.js
@@ -3,7 +3,7 @@ const CustomContent = require('./util/CustomContent')
 module.exports = Object.assign( {}, require('./__proto__'), CustomContent, {
 
     tables: [ 
-        { name: 'Staff', el: 'staff', template: 'staffProfile' }
+        { name: 'Staff', el: 'staff', sort: { 'order': 1 }, template: 'staffProfile' }
     ],
 
     templates: {
