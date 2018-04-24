b719f2b0249a48130bff461f7885adae2a48db35
ScottAP108 <saparton@gmail.com>
2018-04-24 10:12:34 -0700

    secondary email for accounts section

--- models/AccountInfo.js
+++ models/AccountInfo.js
@@ -3,6 +3,12 @@ module.exports = {
         require('./Name'),
         require('./Email'),
         {
+            name: 'secondaryEmail',
+            label: 'Secondary Email',
+            range: 'Email',
+            error: 'A valid email address is required',
+            validate: function( val ) { return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test( val ) }
+        }, {
             name: 'phonenumber',
             label: 'Phone Number',
             range: 'String',
