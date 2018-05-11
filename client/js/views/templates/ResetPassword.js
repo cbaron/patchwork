c925d21f4d90820ec24206988c02c5d491e03062
ScottAP108 <saparton@gmail.com>
2018-05-10 19:37:40 -0700

    css fix for reset password link

--- client/js/views/templates/ResetPassword.js
+++ client/js/views/templates/ResetPassword.js
@@ -1,6 +1,8 @@
 module.exports = p =>
 `<div>
-    <div class="form-wrap">
-        <div data-view="form" data-name="resetPassword"</div>
+    <div>
+        <div class="form-wrap">
+            <div data-view="form" data-name="resetPassword"</div>
+        </div>
     </div>
 </div>`
