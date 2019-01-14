f19cadfd91833ad45dd8ab9eaee57b7ed7728fc5
ScottAP108 <saparton@gmail.com>
2019-01-13 18:48:42 -0800

    minor

--- templates/UnsubscribeMessage.js
+++ templates/UnsubscribeMessage.js
@@ -1,4 +1,4 @@
 module.exports = ({ url }) =>
-`<div style="padding: .25rem 1rem; color: #222; background-color: #fff5df; border-top: 1px solid #ccc;">
+`<div style="padding: .25rem 0; color: #222; background-color: #fff5df; border-top: 1px solid #ccc;">
     <p>If you no longer wish to receive these emails, you may unsubscribe <a href="${url}/unsubscribe">HERE</a></p>
 </div>`
